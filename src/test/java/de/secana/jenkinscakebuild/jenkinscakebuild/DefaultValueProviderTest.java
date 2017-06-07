f9549ae588398f5517c4b6ef9574756a91cf0a35
stefan.hausotte <stefan.hausotte@gdata.de>
2017-06-07 14:40:51 +0200

    added unit test

--- src/test/java/de/secana/jenkinscakebuild/jenkinscakebuild/DefaultValueProviderTest.java
+++ src/test/java/de/secana/jenkinscakebuild/jenkinscakebuild/DefaultValueProviderTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.*;
 
 public class DefaultValueProviderTest {
     @Test
@@ -18,11 +18,71 @@ public class DefaultValueProviderTest {
     }
 
     @Test
-    public void getBootstrapperScriptName() throws Exception {
+    public void getBootstrapperScriptName_isLinux_ReturnsShellScript() {
+        OSChecker osChecker = mock(OSChecker.class);
+        when(osChecker.IsLinux()).thenReturn(true);
+        DefaultValueProvider defaultValueProvider = new DefaultValueProvider(osChecker);
+
+        String bootstrapper = defaultValueProvider.GetBootstrapperScriptName();
+
+        assertEquals("build.sh", bootstrapper);
     }
 
     @Test
-    public void getTargetParameter() throws Exception {
+    public void getBootstrapperScriptName_isWindows_ReturnsPSScript() {
+        OSChecker osChecker = mock(OSChecker.class);
+        when(osChecker.IsWindows()).thenReturn(true);
+        DefaultValueProvider defaultValueProvider = new DefaultValueProvider(osChecker);
+
+        String bootstrapper = defaultValueProvider.GetBootstrapperScriptName();
+
+        assertEquals("build.ps1", bootstrapper);
+    }
+
+    @Test
+    public void getBootstrapperScriptName_isUnknown_ReturnsShellScript() {
+        OSChecker osChecker = mock(OSChecker.class);
+        when(osChecker.IsLinux()).thenReturn(false);
+        when(osChecker.IsWindows()).thenReturn(false);
+        DefaultValueProvider defaultValueProvider = new DefaultValueProvider(osChecker);
+
+        String bootstrapper = defaultValueProvider.GetBootstrapperScriptName();
+
+        assertEquals("build.sh", bootstrapper);
+    }
+
+    @Test
+    public void getTargetParameter_isLinux_ReturnsShellParam() {
+        OSChecker osChecker = mock(OSChecker.class);
+        when(osChecker.IsLinux()).thenReturn(true);
+        DefaultValueProvider defaultValueProvider = new DefaultValueProvider(osChecker);
+
+        String targetParameter = defaultValueProvider.GetTargetParameter();
+
+        assertEquals("--target", targetParameter);
+    }
+
+    @Test
+    public void getTargetParameter_isWindows_ReturnsPSParam() {
+        OSChecker osChecker = mock(OSChecker.class);
+        when(osChecker.IsWindows()).thenReturn(true);
+        DefaultValueProvider defaultValueProvider = new DefaultValueProvider(osChecker);
+
+        String targetParameter = defaultValueProvider.GetTargetParameter();
+
+        assertEquals("-Target", targetParameter);
+    }
+
+    @Test
+    public void getTargetParameter_isUnknown_ReturnsShellParam() {
+        OSChecker osChecker = mock(OSChecker.class);
+        when(osChecker.IsWindows()).thenReturn(false);
+        when(osChecker.IsLinux()).thenReturn(false);
+        DefaultValueProvider defaultValueProvider = new DefaultValueProvider(osChecker);
+
+        String targetParameter = defaultValueProvider.GetTargetParameter();
+
+        assertEquals("--target", targetParameter);
     }
 
 }
