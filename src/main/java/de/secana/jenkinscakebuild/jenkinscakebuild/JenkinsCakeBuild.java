a440f0cb2e7afa2658ea870dd975ecfb2da537b3
stefan.hausotte <stefan.hausotte@gdata.de>
2017-06-07 12:56:40 +0200

    Renamed the main class

--- src/main/java/de/secana/jenkinscakebuild/jenkinscakebuild/JenkinsCakeBuild.java
+++ src/main/java/de/secana/jenkinscakebuild/jenkinscakebuild/JenkinsCakeBuild.java
@@ -23,7 +23,7 @@ import java.io.IOException;
  * <p>
  * When the user configures the project and enables this builder,
  * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link HelloWorldBuilder} is created. The created
+ * and a new {@link JenkinsCakeBuild} is created. The created
  * instance is persisted to the project configuration XML by using
  * XStream, so this allows you to use instance fields (like {@link #name})
  * to remember the configuration.
@@ -33,13 +33,13 @@ import java.io.IOException;
  *
  * @author Kohsuke Kawaguchi
  */
-public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
+public class JenkinsCakeBuild extends Builder implements SimpleBuildStep {
 
     private final String name;
 
     // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
-    public HelloWorldBuilder(String name) {
+    public JenkinsCakeBuild(String name) {
         this.name = name;
     }
 
@@ -71,7 +71,7 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
     }
 
     /**
-     * Descriptor for {@link HelloWorldBuilder}. Used as a singleton.
+     * Descriptor for {@link JenkinsCakeBuild}. Used as a singleton.
      * The class is marked as public so that it can be accessed from views.
      *
      * <p>
